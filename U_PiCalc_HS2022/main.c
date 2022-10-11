d1a397afa637fc4fec3aff092261f2e2b9aebc12
itenpascal <90401249+itenpascal@users.noreply.github.com>
2022-10-11 21:20:38 +0200

    zweiter Algorithmus eingefügt, Tasten eingefügt, umschaltung Algorithmen eingefügt, Fehlerbehebungen Allgemein

--- U_PiCalc_HS2022/main.c
+++ U_PiCalc_HS2022/main.c
@@ -34,16 +34,16 @@
 void vControllerTask(void* pvParameters);
 void vDisplayTask(void* pvParameters);
 void vLeibnizTask(void* pvParameters);
-//void vKettenTask(void* pvParameters);
+void vKettenTask(void* pvParameters);
 //void vTimeTask(void* pvParameters);
 
-// TaskHandle_t	control;
-// TaskHandle_t	display;
-//TaskHandle_t	leibniz;
-//TaskHandle_t	ketten;
+TaskHandle_t	control;
+TaskHandle_t	display;
+TaskHandle_t	leibniz;
+TaskHandle_t	ketten;
 //TaskHandle_t	time;
 
-/*
+
 //EventGroup for ButtonEvents.
 EventGroupHandle_t egButtonEvents = NULL;
 #define BUTTON1_SHORT	0x01
@@ -53,107 +53,143 @@ EventGroupHandle_t egButtonEvents = NULL;
 #define BUTTON3_SHORT	0x10
 #define BUTTON3_LONG	0x20
 #define BUTTON4_SHORT	0x40
-#define BUTTON4_LONG	0x80*/
+#define BUTTON4_LONG	0x80
 #define BUTTON_ALL		0xFF
 
-float pi4 = 1;									//Startwert für Leibnizberechnung
-float pi = 0;
-uint32_t n = 3;									//Nenner Leibnizzahl startwert; unsigned int 32Bit
-char pistring[12];
+float pi = 1;									//Startwert für Leibnizberechnung
 
+uint32_t n = 3;									//Nenner Leibnizzahl startwert; unsigned int 32Bit
+//char pistring[12];
 
 int main(void) {
 	vInitClock();
 	vInitDisplay();
 	
-	xTaskCreate(vControllerTask, (const char *) "control_tsk", configMINIMAL_STACK_SIZE+150, NULL, 3, NULL);		//höchste Prio da Controller
-	xTaskCreate(vDisplayTask, (const char *) "display_tsk", configMINIMAL_STACK_SIZE+150, NULL, 2, NULL);			//Prio 1 da wenig kritisch
-	xTaskCreate(vLeibnizTask, (const char *) "leibniz_tsk", configMINIMAL_STACK_SIZE+150, NULL, 1, NULL);			//Prio 1,
-	//xTaskCreate(vKettenTask, (const char *) "ketten_tsk", configMINIMAL_STACK_SIZE+150, NULL, 1, &ketten);				//
+	xTaskCreate(vControllerTask, (const char *) "control_tsk", configMINIMAL_STACK_SIZE+150, NULL, 3, &control);		//höchste Prio da Controller
+	xTaskCreate(vDisplayTask, (const char *) "display_tsk", configMINIMAL_STACK_SIZE+150, NULL, 2, &display);			//Prio 1 da wenig kritisch
+	xTaskCreate(vLeibnizTask, (const char *) "leibniz_tsk", configMINIMAL_STACK_SIZE+150, NULL, 1, &leibniz);			//Prio 1,
+	xTaskCreate(vKettenTask, (const char *) "ketten_tsk", configMINIMAL_STACK_SIZE+150, NULL, 1, &ketten);				//
 	//xTaskCreate(vTimeTask, (const char *) "time_tsk", configMINIMAL_STACK_SIZE+150, NULL, 2, &time);					//
 	
 	vDisplayClear();
 	vDisplayWriteStringAtPos(0,0,"PI-Calc HS2022");
 	vDisplayWriteStringAtPos(1,0,"leer1");							//
 	vDisplayWriteStringAtPos(2,0,"leer2");							//
-	vDisplayWriteStringAtPos(3,0,"Strt|Stp|Rst|switch");			//
+	vDisplayWriteStringAtPos(3,0,"strt|stp|reset|swtch");			//
 	//vTaskDelay(10);
 	vTaskStartScheduler();
 	return 0;
 }
 
 void vControllerTask(void* pvParameters) {
-	initButtons();
+	egButtonEvents = xEventGroupCreate();
+	initButtons(); //Initialize Buttons
 	for(;;) {
-		vDisplayClear();
+		//vDisplayClear();														// aktuell auskommentiert, da dazwischenfunken mit der Anzeige
 		updateButtons();
-		if(getButtonPress(BUTTON1) == SHORT_PRESSED) {
-			//char pistring[12];
-			//sprintf(&pistring[0], "PI: %.8f", M_PI);
-			//vDisplayWriteStringAtPos(1,0, "%s", pistring);
+		if(getButtonPress(BUTTON1) == SHORT_PRESSED) {							// Start
+			xEventGroupSetBits(egButtonEvents, BUTTON1_SHORT);
+																										//char pistring[12];
+																										//sprintf(&pistring[0], "PI: %.8f", M_PI);
+																										//vDisplayWriteStringAtPos(1,0, "%s", pistring);
 		}
-		if(getButtonPress(BUTTON2) == SHORT_PRESSED) {
-			
+		if(getButtonPress(BUTTON2) == SHORT_PRESSED) {							// Stopp - Unterbrechen
+			xEventGroupSetBits(egButtonEvents, BUTTON2_SHORT);
 		}
-		if(getButtonPress(BUTTON3) == SHORT_PRESSED) {
-			
+		if(getButtonPress(BUTTON3) == SHORT_PRESSED) {							// Reset der Berechnung der Zahl Pi
+			xEventGroupSetBits(egButtonEvents, BUTTON3_SHORT);
 		}
-		if(getButtonPress(BUTTON4) == SHORT_PRESSED) {							//Wechsel der Algorithmen durch inaktiv setzen des jeweilig anderen
-			
+		if(getButtonPress(BUTTON4) == SHORT_PRESSED) {							// Switch - Wechsel der Algorithmen durch Suspend setzen des jeweilig anderen
+			//xEventGroupSetBits(egButtonEvents, BUTTON4_SHORT);
+			eTaskState state = eTaskGetState(leibniz);							// HandleName und nicht TaskName
+			if(state == eSuspended) {
+				vTaskSuspend(ketten);
+				vTaskResume(leibniz);
+				
+	
+			} else {
+				vTaskSuspend(leibniz);
+				vTaskResume(ketten);
+				
+			}
 		}
 		if(getButtonPress(BUTTON1) == LONG_PRESSED) {
-			
+			xEventGroupSetBits(egButtonEvents, BUTTON1_LONG);
 		}
 		if(getButtonPress(BUTTON2) == LONG_PRESSED) {
-			
+			xEventGroupSetBits(egButtonEvents, BUTTON2_LONG);
 		}
 		if(getButtonPress(BUTTON3) == LONG_PRESSED) {
-			
+			xEventGroupSetBits(egButtonEvents, BUTTON3_LONG);
 		}
 		if(getButtonPress(BUTTON4) == LONG_PRESSED) {
-			
+			xEventGroupSetBits(egButtonEvents, BUTTON4_LONG);
 		}
 		//vTaskDelay(10/portTICK_RATE_MS);
-		vTaskDelay(100);
+		vTaskDelay(10);															// alle 10ms abfragen
 	}
 }
 
 void vDisplayTask(void* pvParameters) {
 	char temp[12] = "";
 	for (;;) {
-		sprintf(temp,"%.8f",pi4 * 4);
-		//sprintf(&pistring[12], "PI: %.8f", pi4*4);
+		sprintf(temp,"%.8f",pi);																//Datenabholung muss berechnung stoppen, abholen, weiterlaufen
+		//sprintf(&pistring[12], "PI: %.8f", pi*4);
 		vDisplayClear();												//clear Display vor neuschreiben
 		vDisplayWriteStringAtPos(0,0,"Titel");							//Zeile,
-		vDisplayWriteStringAtPos(1,0,"leer1");							//
-		vDisplayWriteStringAtPos(2,0,"leer2345");					//
-		vDisplayWriteStringAtPos(3,0, "%s", temp);					//
-		vTaskDelay(200);
+		vDisplayWriteStringAtPos(1,0,"Algorithmus");					//Leibniz Ketten
+		vDisplayWriteStringAtPos(2,0, "PI:    %s", temp);				//
+		vDisplayWriteStringAtPos(3,0, "strt|stp|reset|swtch");			// Start | Stopp | Reset | Switch
+		vTaskDelay(500);
 	}
 }
 
-void vLeibnizTask(void* pvParameters) {					//Berechnung Pi/4
-	float piHilfe = pi4;
+void vLeibnizTask(void* pvParameters) {					//Berechnung Pi/4								//auslesen in blockmeldung mit Eventbit(waitbit), Mit Suspense moden umschalten
+	//vTaskSuspend(leibniz);											//initial state of vKettenTask Task shall be off
+	float piHilfe = 1;
+	
 	for(;;) {
 		piHilfe = piHilfe - (1.0/n);				//1.0 nötig dass es float ist, bei nur 1 istes Int
-		n = n + 2;
-		piHilfe = piHilfe + (1.0/n);
-		pi4 = piHilfe;
-		n = n + 2;
-		vTaskDelay(1);
+			n = n + 2;
+			piHilfe = piHilfe + (1.0/n);
+			pi = piHilfe*4;
+			n = n + 2;
+	}
+}
+
+void vKettenTask(void* pvParameters) {									// https://matheguru.com/allgemein/die-kreiszahl-pi.html
+	vTaskSuspend(ketten);											//initial state of vKettenTask Task shall be off
+	float piHilfe = 2;
+	float piSave = 1;
+	
+	for(;;) {
+		piSave = piSave*(piHilfe/(piHilfe - 1));
+		piSave = piSave*(piHilfe/(piHilfe + 1));
+		piHilfe = piHilfe + 2;
+		pi = piSave*2;
 	}
 }
 /*
-void vKettenTask(void* pvParameters) {
-	//float Pi
-	for(;;) {
-		
-		vTaskDelay(100);
-	}
-}
-
-void vTimeTask(void* pvParameters) {			//PI = 3.141592653589793
+void vTimeTask(void* pvParameters) {			//PI = 3.14159 2653589793
 	for(;;) {
 		vTaskDelay(100 / portTICK_RATE_MS);
 	}
 }*/
+
+
+/*
+if(xEventGroupGetBits(egButtonEvents) & BUTTON2_SHORT) {
+*/
+
+/*
+			eTaskState state = eTaskGetState(blinkTask1Handle);
+			if(state == eSuspended) {
+				vTaskResume(blinkTask1Handle);
+			} else {
+				vTaskSuspend(blinkTask1Handle);
+			}
+			*/
+
+/*
+	vTaskSuspend(loadkillTaskHandle); //initial state of loadkill Task shall be off
+*/
